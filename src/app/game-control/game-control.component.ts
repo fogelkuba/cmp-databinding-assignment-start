99b95b4ab39ff5e0e6c097d82460ceb58e570c48
fogelkuba <fogelkuba@gmail.com>
2018-05-20 12:07:46 +0200

    fix

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+
+  onStartGame(){
+    window.clearInterval(this.interval);
+    this.interval = setInterval(
+      ()=> {
+        this.intervalFired.emit(this.lastNumber + 1);
+        this.lastNumber++;
+        console.log(this.lastNumber);
+      },
+      1000)
+  };
+
+  onStopGame(){
+    window.clearInterval(this.interval);
+  }
 
   constructor() { }
 
